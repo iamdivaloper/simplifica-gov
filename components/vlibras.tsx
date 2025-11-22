096dc754b010bcfb1e3e0b45de7ee32635217493
Jessica Lopes <divaloper@MacBook-Air-de-Jessica.local>
2025-11-22 09:44:02 -0300

    feat: Integrate VLibras widget with styling and script loading adjustments, update Vercel Analytics, and refine TypeScript and project configuration

--- components/vlibras.tsx
+++ components/vlibras.tsx
@@ -5,15 +5,18 @@ import Script from "next/script"
 export function VLibras() {
   return (
     <>
+      {/* @ts-ignore */}
       <div vw="true" className="enabled">
+        {/* @ts-ignore */}
         <div vw-access-button="true" className="active"></div>
+        {/* @ts-ignore */}
         <div vw-plugin-wrapper="true">
           <div className="vw-plugin-top-wrapper"></div>
         </div>
       </div>
       <Script
         src="https://vlibras.gov.br/app/vlibras-plugin.js"
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         onLoad={() => {
           // @ts-ignore
           new window.VLibras.Widget("https://vlibras.gov.br/app")
